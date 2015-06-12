df5026ca884cd0f88e1fca9dcf1e4dbfd0ee5d24
theGeekCircle <theGeekCircle@gmail.com>
2015-06-12 14:02:56 +0100

    Added basic test code
    Added menu file, importing a font and displaying "Hello World!" as 100,
    100. Added gamestate variable and esc to quit code to main.lua. Also
    added LeagueGothic.ttf font.

--- main.lua
+++ main.lua
@@ -1,13 +1,19 @@
 require "player"
+require "menu"
 
 function love.load()
-
+	gamestate = "menu"
+	menu.load()
 end
 
 function love.update()
-
+	if love.keyboard.isDown('escape') then
+		love.event.quit()
+	end
 end
 
 function love.draw()
-
+	if gamestate == "menu" then
+		menu.draw()
+	end
 end
